b45ec272dd45db38cfe45953896f16178f3cfc78
OleksandrMaimeskul <maymeskulsasha@gmail.com>
2024-03-09 16:46:23 +0200

    2. Retake correction

--- src/Lesson05Homework09.java
+++ src/Lesson05Homework09.java
@@ -4,27 +4,28 @@ import java.util.Scanner;
 public class Lesson05Homework09 {
     public static void main(String[] args) {
         Random random = new Random();
-        int pcNumber = random.nextInt(11 );
-        int totalAttempts = 3;
+        int pcNumber = random.nextInt(11);
+        int totalAttempts = 5;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Let's guess the number from 0 to 10. " +
-                "You have 3 attempts.");
+                "You have 5 attempts.");
         while (totalAttempts > 0) {
             System.out.println("Enter your number.");
             int guessed = scanner.nextInt();
+            totalAttempts--;
             if (guessed > 10) {
                 System.out.println("Out of range!");
             } else if (guessed < 0) {
                 System.out.println("Out of range!");
             } else if (guessed == pcNumber) {
                 System.out.println("Congratulation you win!");
+                break;
             } else {
                 System.out.println("Not correct, please try again!");
-                totalAttempts--;
             }
         }
-        System.out.println("No more attempts, please try your lucky next time....");
+        System.out.println("No more attempts.");
         scanner.close();
     }
 }
